62bd1f8bdc997f3941330a139588a50dc7a49adf
Rob Quist <rob@enrise.com>
2013-07-30 17:04:14 +0200

    edit configuration

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,8 +7,9 @@ class bind9::config (
   if ($bindtype == 'master') {
     
     $masterslavetext = "allow-update { key \"DDNS_DHCP\"; ${slaveips} };
-        allow-transfer { ${slaveips} };
-        also-notify { ${slaveips} };"
+    allow-transfer { ${slaveips} };
+    also-notify { ${slaveips} };
+    notify yes;"
 
 	  file { '/var/lib/bind':
 	    ensure    => directory,
@@ -18,16 +19,30 @@ class bind9::config (
 	    group   => 'bind',
 	    source    => $bind9::zonefolder,
 	  }
-  } else { #Create the folder but don't fill it yet
-    $masterslavetext = "masters { ${masterips} }"
+  } else { #Create the folders but don't fill them yet
+    $masterslavetext = "masters { ${masterips} };"
 
     file { '/var/lib/bind':
       ensure    => directory,
-      recurse   => true,
-      purge     => false,
       owner   => 'bind',
       group   => 'bind',
     }
+    
+    file { '/var/lib/bind/common':
+      ensure    => directory,
+      owner   => 'bind',
+      group   => 'bind',
+    }
+    
+    file { '/var/lib/bind/internal':
+      ensure    => directory,
+      owner   => 'bind',
+      group   => 'bind',
+    }
+    
+    File ['/var/lib/bind'] -> File['/var/lib/bind/common']
+    File ['/var/lib/bind/common'] -> File['/var/lib/bind/internal']
+    
   }
 
   file { '/etc/bind':
